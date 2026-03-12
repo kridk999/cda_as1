b1f2a83c405d92ed725fe4051d9caee78cecd20b
kridk999 <s214609@student.dtu.dk>
2026-03-12 10:27:40 +0100

    modified train to fit with data.py

--- src/cda/train.py
+++ src/cda/train.py
@@ -4,35 +4,38 @@ from sklearn.model_selection import KFold
 from sklearn import linear_model
 import warnings
 import matplotlib.pyplot as plt
+from data import preprocess_data
 
 
 data = pd.read_csv('data/case1Data.csv').values
 y = data[:, 0]
 X = data[:, 1:]
 
+X_train, y_train, preprocessor = preprocess_data(data)
+
 CV = 5
 kf = KFold(n_splits=CV)
 
-[n,p] = X.shape
+[n,p] = X_train.shape
 lambdas = np.logspace(-3, 1, num=20)
 alpha = 0.001
 
 coefs = np.zeros((CV, len(lambdas), p))
 
-
-for i, (train_index, test_index) in enumerate(kf.split(X)):
-    ytrain = y[train_index].ravel() 
-    Xtrain = X[train_index]
     
-    # <-- Apply Custom Imputation HERE
-    # Ensure my_custom_imputer returns the imputed numpy array
-    #Xtrain = my_custom_imputer(Xtrain)
+for i, (train_index, test_index) in enumerate(kf.split(X)):
+    #ytrain = y[train_index].ravel() 
+    #Xtrain = X[train_index]
+    
+    df_train = data.iloc[train_index]
+    
+    X_train, y_train, preprocessor = preprocess_data(df_train)
     
     for j, lambda_ in enumerate(lambdas):
         with warnings.catch_warnings(): 
             warnings.simplefilter("ignore")
 
-            model = linear_model.ElasticNet(l1_ratio=alpha, alpha=lambda_, normalize=False).fit(Xtrain, ytrain)
+            model = linear_model.ElasticNet(l1_ratio=alpha, alpha=lambda_, normalize=False).fit(X_train, y_train)
             coefs[i,j,:] = model.coef_
         
 trace = np.sum(coefs, axis=0)
